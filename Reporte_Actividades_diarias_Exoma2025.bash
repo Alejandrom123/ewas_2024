387dcefc34135b35f3882aa6f405855fb03f2f6d
Alejandrom123 <153859726+Alejandrom123@users.noreply.github.com>
2025-06-19 14:59:06 -0500

    Add files via upload

--- Reporte_Actividades_diarias_Exoma2025.bash
+++ Reporte_Actividades_diarias_Exoma2025.bash
@@ -1,9 +1,32 @@
+#########################################################################
+######################### Indicaciones iniciales##############################
+#########################################################################
+
 IIIII Funcionó
 IIIII No funcionó
 IIIII Problemas
 
-Ubicación
+https://bio.tools/VCF-Server
+http://diseasegps.sjtu.edu.cn/VCF-Server?lan=eng
+
+Elementos para abrir para trabajar en exoma: 
+Reporte_Actividades_diarias_Exoma2025 (word) y el .sh con este mismo nombre lo completo al final del día para subir los cambios hechos en GIT
+
+El pipeline de exoma en el que estoy trabajando 
+C:\Users\User\Desktop\exoma\Script_exoma.bash
+
+A fecha de 17-06-2025 lo que estoy trabajando está en el servidor de biología
+/mnt/disc2/grupobcei/ewas/ewas_Acacias/02.1_ewas_trimmed_subsample/
+
+Ubicación de lo de EWAS en general
 #Samples in /mnt/disc2/grupobcei/ewas/ in 172.16.0.96 (grupobcei) server
+
+
+#########################################################################
+################################################################
+#########################################################################
+
+
 
 Script Python para mapear automáticamente 
 "/mnt/disc2/grupobcei/ewas/ewas_Acacias/02.1_ewas_trimmed_subsample/automatizt/bucle_mapp_bowtie.py"
@@ -19,7 +42,10 @@ Al realizar el script por separado, los porcentajes son los mismos. Procedo a ha
 	Realicé el index con BWA
 	Realicé el bucle del mapeo con BWA, hay que correrlo mañana
 
-10-06-2025
+#########################################################################
+###############################10-06-2025#################################
+#########################################################################
+
 IA y pipeline Johana Tejada
 Muestras HLR1 (HLR2) y HPR1 (HPR2)
 
@@ -53,12 +79,10 @@ Marcar duplicados usando picard
 #Gatk filtering
 nohup /mnt/disc2/grupobcei/java/jdk-17.0.12/bin/java -jar /mnt/disc2/grupobcei/gatk/gatk-4.6.1.0/gatk-package-4.6.1.0-local.jar VariantFiltration -R /mnt/disc2/grupobcei/ewas/index/VectorBase-68_AaegyptiLVP_AGWG_Genome.fasta -V HLR1.variants.vcf --filter-name FAIL --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || DP < 10 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" -O HLR1_filt_10x.vcf
 
-/mnt/disc2/grupobcei/ewas/ewas_Acacias/bcftools/bcftools-1.22/bcftools view HLR1_filt_10x.vcf --threads 30 -Oz -O HLR1_filt_10x.vcf.gz
-
-## Generate before/after plots [Only necessary if you want to look at the data; requires R libraries ggplot2, reshape, gplots, gsalib]
-nohup /mnt/disc2/grupobcei/java/jdk-17.0.12/bin/java -jar /mnt/disc2/grupobcei/gatk/gatk-4.6.1.0/gatk-package-4.6.1.0-local.jar AnalyzeCovariates -R /mnt/disc2/grupobcei/ewas/index/VectorBase-68_AaegyptiLVP_AGWG_Genome.fasta -V HLR1.variants.vcf -before HLR1before_recal_data.table -after HLR1_postRecal_data.table -plots HLR1_recalibration_plots.pdf
+/mnt/disc2/grupobcei/ewas/ewas_Acacias/bcftools/bcftools-1.22/bcftools view HLR1_filt_10x.vcf --threads 30 -Oz -O HLR1_filt_10x.vcf.gz 
 
 # SnpEff for SNP annotation
+
 Genoma de referencia en snpeff
 Aedes_aegypti_lvpagwg
 
@@ -67,8 +91,68 @@ java -jar /data1/softwares/snpEff/snpEff.jar Aedes_aegypti_lvpagwg -c /data1/sof
 /mnt/disc2/grupobcei/java/jdk-24.0.1/bin/java -jar /mnt/disc2/grupobcei/ewas/ewas_Acacias/snpEff/snpEff.jar Aedes_aegypti_lvpagwg -c /mnt/disc2/grupobcei/ewas/ewas_Acacias/snpEff/snpEff.config HLR1_filt_10x.vcf > HLR1_GATK_annotated.vcf 
 
 
+#########################################################################
+###############################12-06-2025#################################
+#########################################################################
 
-########################################################################
+Realicé ordenamiento de script en Script_exoma
+Y realicé samtools bucle para view, sort y luego index. 
+Se deja corriendo
+
+#########################################################################
+###############################17-06-2025#################################
+#########################################################################
+
+
+Comando luego de samtools es read groups: 
+/mnt/disc2/grupobcei/java/jdk-17.0.12/bin/java -jar /mnt/disc2/grupobcei/picard/picard.jar AddOrReplaceReadGroups I=HLR1_sbs_BWA_sortd.bam O=HLR1_BWA_rg.bam SO=coordinate CREATE_INDEX=true RGID=HLR1 RGLB=lib1 RGPL=illumina RGPU=HLR1 RGSM=sample1
+
+
+#########################################################################
+###############################18-06-2025#################################
+#########################################################################
+
+
+Creé el archivo de métricas usando samtools stat:
+nohup bash -c 'for file in *.sam; do samtools stats "$file" > "${file%.sam}.stats.txt"; done' > nohup.out 2>&1 &  
+
+y luego para unir:
+cat *stats.txt > merged_BWA_stats.txt
+
+samtools stats HLR1_subsample_BWA.sam > HLR1_bwa_stats.txt
+
+En archivo métricas C:\Users\User\Desktop\exoma\outputs_mobaxterm\Metricas_Mapeo_Exoma.xlsx
+Están las métricas del mapeo
+
+La muestra HLR1 parece que está mala.  
+
+#########################################################################
+###############################19-06-2025#################################
+#########################################################################
+
+Hay un problema con HLR1. Cuando mapeo con BWA, dice que el header está malo. 
+Estoy mirando si se comparte con HLS1. 
+
+Pasar a BAM a ver sí samtools lo reconoce más fácilmente. 
+
+--- 
+Estos son los comandos: 
+samtools view -bS HLR1_BWA_19-06-25.sam > HLR1_BWA_19-06-25.bam 
+samtools flagstat HLR1_BWA_19-06-25.bam 
+samtools stats HLR1_BWA_19-06-25.bam > HLR1_BWA_19-06-25.stats.txt
+
+Volví a cargar la muestra HLR1 desde mi tera y funcionó. HLR1 percentage of properly paired reads (%):	82.1
+
+/mnt/disc2/grupobcei/ewas/ewas_Acacias/02.1_ewas_trimmed_subsample/
+
+
+
+
+
+#########################################################################
+###############################Restantes#################################
+#########################################################################
+
 ## Index the BAM file using Picard Tools
 /mnt/disc2/grupobcei/java/jdk-17.0.12/bin/java -jar /mnt/disc2/grupobcei/picard/picard.jar BuildBamIndex INPUT= HLR1_BWA_-nodups.bam
 
@@ -81,8 +165,3 @@ gatk --java-options "-Xmx4g" RealignerTargetCreator -R reference.fasta -I HLR1_B
 
 /mnt/disc2/grupobcei/java/jdk-17.0.12/bin/java -Xmx30g -jar /home/administrador/programas/gatk-4.6.1.0/gatk-package-4.6.1.0-local.jar BaseRecalibrator -R /mnt/disc2/grupobcei/ewas/index/VectorBase-68_AaegyptiLVP_AGWG_Genome.fasta -I HLR1_BWA_-nodups.bam -O HLR1_BWA_recal_data.table
 
-#########################################################################
-###############################12-06-2025#################################
-#########################################################################
-
-Realicé ordenamiento de script en Script_exoma
